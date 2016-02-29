f61c395398afdbf96928faca6f9a1180c945110d
Johanny Alexix Sevilla Ortega <johanny.sevilla.sc@itszapopan.edu.mx>
2016-02-29 08:31:07 -0600

    no se agrego nada

--- clase/HolaMundo.java
+++ clase/HolaMundo.java
@@ -1,6 +1,7 @@
 public class HolaMundo{
   public static void main(strings[] args){
     system.out.printl ("Hola Mundo");
-    system.out.printl ("Otra cosa");
+    system.out.printl ("no se les ocurre nada");
+
   }
 }
